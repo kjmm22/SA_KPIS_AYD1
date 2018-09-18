f15bcfb893c327afa2ca1368b33240a6bef0e43b
obatres <batres.93@gmail.com>
2018-09-17 21:31:17 -0600

    Creadas las pruebas unitarias para la calificacion del docente

---
+++ Aplicacion/Aplicacion.Tests/Catedratico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Tests
+{
+    public class Catedratico
+    {
+       
+        public string Nombre { get; set; }
+        public string Codigo { get; set; }
+        public int Calificacion { get; set; }
+        public int Tipo { get; set; }
+
+        public bool validar()
+        {
+            if (Tipo !=3)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
