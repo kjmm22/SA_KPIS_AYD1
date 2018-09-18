0f4e74a1cb496cd546fa543a56d74974b314f129
Orlando Batres <obatres@users.noreply.github.com>
2018-09-17 22:38:11 -0600

    Calificacion catedratico (#21)
    * Pull request Sprint 1 (#19)
    
    * 0.0.0 Proyecto con plantilla
    
    * 0.0.0 Proyecto con plantilla y TDD
    
    * 0.0.1 Fix plantilla
    
    Se arregla plantilla
    
    * Creada la pagina de CRUD de Grados
    Establecida la conexion con la base de datos
    Creacion de Grados hecha
    
    * Crud grados (#15)
    
    * 0.0.0 Proyecto con plantilla
    
    * 0.0.0 Proyecto con plantilla y TDD
    
    * 0.0.1 Fix plantilla
    
    Se arregla plantilla
    
    * Creada la pagina de CRUD de Grados
    Establecida la conexion con la base de datos
    Creacion de Grados hecha
    
    * Agregada las funciones de eliminar y modificar al CRUD de grados
    
    * Creadas las paginas de Secciones y ciclos
    
    * CRUD de secciones y CRUD de ciclos agregado
    
    * Planificacion (#16)
    
    * 0.0.0 Proyecto con plantilla
    
    * 0.0.0 Proyecto con plantilla y TDD
    
    * 0.0.1 Fix plantilla
    
    Se arregla plantilla
    
    * CRUD Planificacion
    
    * CRUD Planificacion
    
    Se agrega lectura de planes aprobados, rechazados y por aprobar
    
    * Se arreglan problemas con visualización de actividades
    
    * Agregar actividades
    
    Se agregan botones y labels
    
    * CRUD Planificacion Profesor
    
    CRUD completo
    
    * CRUD Planificacion completo
    
    Maestro y Director
    
    * Merge branch 'origin/CRUDSalon' into develop
    
    
    Merge remote-tracking branch 'origin/develop' into develop
    
    * Interface (#17)
    
    * 0.0.0 Proyecto con plantilla
    
    * 0.0.0 Proyecto con plantilla y TDD
    
    * 0.0.1 Fix plantilla
    
    Se arregla plantilla
    
    * Crud grados (#14)
    
    * Creada la pagina de CRUD de Grados
    Establecida la conexion con la base de datos
    Creacion de Grados hecha
    
    * Agregada las funciones de eliminar y modificar al CRUD de grados
    
    * Interface (main)
    
    Cambio de interface en la pantalla inicial del proyecto (solo banner)
    
    * Interface (Otros master)
    
    Se actualizo el banner de los master para distintos tipos de usuarios
    
    * Commit 08-sep
    
    Quitar archivos css inecesarios.
    
    * Commit 08-sep (2)
    
    Página de inicio con más información acerca de la herramienta. (todavia no esta completa)
    
    * Commit 09-sep (1)
    
    Resto de paginas creadas, iniciando la validación de usuarios, nota de error por datos incorrectos.
    
    * Commit 09-sep (1.1)
    
    Eliminación de una línea de más.
    
    * Commit 10-sep (1.0)
    
    La funcionalidad se sesiones ha sido añadida, sin hacer la validación o verificación de usuarios (no hay conexion con la base de datos todavia).
    Redirección hacia la pagina principal si ningun usuario no ha iniciado sesión.
    
    * Commit 10-sep (1.1)
    
    Se agregaron validaciones para el manejo de usuarios.
    
    * Crud profesores (#18)
    
    * 0.0.0 Proyecto con plantilla
    
    * 0.0.0 Proyecto con plantilla y TDD
    
    * 0.0.1 Fix plantilla
    
    Se arregla plantilla
    
    * Crud grados (#14)
    
    * Creada la pagina de CRUD de Grados
    Establecida la conexion con la base de datos
    Creacion de Grados hecha
    
    * Agregada las funciones de eliminar y modificar al CRUD de grados
    
    * CRUD_Profesores_201114210
    
    * CRUD_Profesores#2
    
    * commit_Temp
    
    * Modificacion_Cambio de pagina CRUDPROFESORES
    
    * Visualizar profesores inscritos en la institucion
    
    * Insertar profesores
    
    * Commit Final CRUDProfesores
    
    * Master sin errores
    
    El anterior commit contenía errores, por favor de darle fetch
    
    * Creadas las clases para los test de la calificacion de maestros
    
    * Creadas las pruebas unitarias para la calificacion del docente

---
+++ Aplicacion/Aplicacion.Tests/CatedraticoTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Aplicacion.Tests
+{
+    [TestClass]
+    public class CatedraticoTest
+    {
+        [TestMethod]
+        public void Validar()
+        {
+            Catedratico n = new Catedratico
+            {
+                Tipo = 3 
+            };
+            bool resul = n.validar();
+            Assert.IsTrue(resul);
+
+        }
+    }
+}
