f6368984e2e6357cc2e74afa32f5d14f3a892e08
Cristian <moino.cristian@gmail.com>
2018-10-18 08:34:47 -0600

    201212734
    201212734-creacion clases test restriccion

---
+++ Aplicacion/AYD1_Aplicacion.Tests/TestRestriccion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AYD1_Aplicacion.Tests
+{
+    [TestClass]
+    public class TestRestriccion
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
