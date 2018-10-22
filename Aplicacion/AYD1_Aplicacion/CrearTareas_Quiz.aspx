ce758ed73b57b9a2dfdcaaa043187df614626cbb
201114210 <abraham.elias.elias@gmail.com>
2018-10-22 10:48:10 -0600

    201122998

--- Aplicacion/AYD1_Aplicacion/CrearTareas_Quiz.aspx
+++ Aplicacion/AYD1_Aplicacion/CrearTareas_Quiz.aspx
@@ -1,5 +1,74 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PaginasMaster/ma_admin_g8.Master" AutoEventWireup="true" CodeBehind="CrearTareas_Quiz.aspx.cs" Inherits="AYD1_Aplicacion.CrearTareas_Quiz" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+        .auto-style1 {
+            width: 565px;
+        }
+        .auto-style2 {
+            width: 194px;
+        }
+    </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        Creacion de Tareas:</p>
+    <p>
+        <Table ID="Table1" runat="server" Height="16px" class="auto-style1">
+            <tr>
+                <td> Agregar Archivo Tareas: </td>
+                <td> 
+                    <asp:FileUpload ID="FileUpload1" runat="server" Width="359px" />
+                </td>
+            </tr>
+            <tr>
+                <td>Describir Tarea:</td>
+                <td>             
+                    <asp:TextBox ID="TextBox1" runat="server" Width="352px"></asp:TextBox>
+                </td>
+            </tr>
+                        <tr>
+                <td>&nbsp;</td>
+                <td>             
+                    <asp:Button ID="Button1" runat="server" Text="Guardar" />
+                            </td>
+            </tr>
+        </Table>
+    </p>
+    <p>
+        Crear Quiz</p>
+    <table ID="Table2" runat="server" Height="16px" class="auto-style1">
+        <tr>
+            <td class="auto-style2">
+                Agregar archivo de Pruebas
+            </td>
+            <td>
+
+                <asp:FileUpload ID="FileUpload2" runat="server" Width="367px" />
+
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style2">
+                Descripcion de la Prueba
+            </td>
+   
+            <td>
+
+                <asp:TextBox ID="TextBox2" runat="server" Width="355px"></asp:TextBox>
+
+            </td>
+        </tr>
+        <tr>
+            <td>
+
+            </td>
+            <td>
+
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="Button2" runat="server" Text="Guardar" />
+
+            </td>
+        </tr>
+    </table>
+    <br />
 </asp:Content>
--- Aplicacion/AYD1_Aplicacion/CrearTareas_Quiz.aspx.designer.cs
+++ Aplicacion/AYD1_Aplicacion/CrearTareas_Quiz.aspx.designer.cs
@@ -11,5 +11,77 @@ namespace AYD1_Aplicacion {
     
     
     public partial class CrearTareas_Quiz {
+        
+        /// <summary>
+        /// Control Table1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTable Table1;
+        
+        /// <summary>
+        /// Control FileUpload1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUpload1;
+        
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Control Table2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTable Table2;
+        
+        /// <summary>
+        /// Control FileUpload2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUpload2;
+        
+        /// <summary>
+        /// Control TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// Control Button2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
     }
 }
