c2b95946a9d77036bc1a3bd33792f744a006e05b
201114210 <abraham.elias.elias@gmail.com>
2018-10-22 10:20:21 -0600

    201114210

---
+++ Aplicacion/AYD1_Aplicacion/CrearTareas_Quiz.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PaginasMaster/ma_admin_g8.Master" AutoEventWireup="true" CodeBehind="CrearTareas_Quiz.aspx.cs" Inherits="AYD1_Aplicacion.CrearTareas_Quiz" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ Aplicacion/AYD1_Aplicacion/CrearTareas_Quiz.aspx.designer.cs
@@ -0,0 +1,15 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace AYD1_Aplicacion {
+    
+    
+    public partial class CrearTareas_Quiz {
+    }
+}
